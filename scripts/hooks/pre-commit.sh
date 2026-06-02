b9ba44cc9e20ec02bc0b9d22959b363681b95c51
Philip Leftwich <philip.leftwich@gmail.com>
2026-06-02 18:23:40 +0100

    Hook test

--- scripts/hooks/pre-commit.sh
+++ scripts/hooks/pre-commit.sh
@@ -1,6 +1,7 @@
-echo "--- Checking renv synchronisation..."
+#!/usr/bin/env bash
 
-Rscript --no-save --no-restore-data -e '
+echo "--- Checking renv synchronisation..."
+"/c/Program Files/R/R-4.5.3/bin/x64/Rscript" --no-save --no-restore-data -e '
   if (!isTRUE(renv::status()$synchronized)) {
     message("\nrenv is out of sync with renv.lock.\nRun renv::snapshot() then commit again.\n")
     quit(status = 1)
